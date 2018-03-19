0053ec8324fcada62a958ae4475b128385cd400f
Jiayuan Mao <maojiayuan@gmail.com>
2018-03-19 20:26:39 +0800

    [master] update JacDataParallel

--- jactorch/parallel/data_parallel.py
+++ jactorch/parallel/data_parallel.py
@@ -6,16 +6,48 @@
 # 
 # This file is part of Jacinle.
 
+import torch.cuda as cuda
+import torch.nn as nn
+from torch.nn.parallel.data_parallel import DataParallel
+
 from jactorch.cuda.copy import async_copy_to
 
-from .dict_gather import DictGatherDataParallel
-from .replication_callback import ReplicationCallbackDataParallel
-from .user_scattered import UserScatteredDataParallel
+from .dict_gather import data_parallel_dict_gather
+from .replicate import replicate
+from .replication_callback import exec_data_parallel_replication_callback
+from .user_scattered import use_user_scattered
 
 __all__ = ['JacDataParallel', 'UserScatteredJacDataParallel']
 
 
-class JacDataParallel(DictGatherDataParallel, ReplicationCallbackDataParallel):
+class JacDataParallel(DataParallel):
+    def __init__(self, module,
+                 device_ids=None, output_device=None, dim=0,
+                 allow_dict=True, allow_replication_callback=True,
+                 user_scattered=False, use_scatter_stream=True,
+                 persistent=False, copy_parameters=False, copy_buffers=True):
+
+        super(DataParallel, self).__init__()
+        if device_ids is None:
+            device_ids = list(range(cuda.device_count()))
+        if output_device is None:
+            output_device = device_ids[0]
+        self.dim = dim
+        self.module = module
+        self.device_ids = device_ids
+        self.output_device = output_device
+        if len(self.device_ids) == 1:
+            self.module.cuda(device_ids[0])
+
+        self.allow_dict = allow_dict
+        self.allow_replication_callback = allow_replication_callback
+        self.user_scattered = user_scattered
+        self.use_scatter_stream = use_scatter_stream
+        self.persistent = persistent
+        self.copy_parameters = copy_parameters
+        self.copy_buffers = copy_buffers
+        self.replicas = nn.ModuleList()
+
     def forward(self, *inputs, **kwargs):
         inputs, kwargs = self.scatter(inputs, kwargs, self.device_ids)
         if len(self.device_ids) == 1:
@@ -26,6 +58,33 @@ class JacDataParallel(DictGatherDataParallel, ReplicationCallbackDataParallel):
         outputs = self.parallel_apply(replicas, inputs, kwargs)
         return self.gather(outputs, self.output_device)
 
+    def scatter(self, inputs, kwargs, device_ids):
+        if self.user_scattered:
+            return use_user_scattered(inputs, kwargs, device_ids, use_stream=self.use_scatter_stream)
+        return super().scatter(inputs, kwargs, device_ids)
 
-class UserScatteredJacDataParallel(JacDataParallel, UserScatteredDataParallel):
-    pass
+    def gather(self, outputs, output_device):
+        if self.allow_dict:
+            return data_parallel_dict_gather(self, outputs, output_device)
+        return super().gather(outputs, output_device)
+
+    def replicate(self, module, device_ids):
+        if self.persistent or len(self.replicas) == 0:
+            if not self.persistent:
+                modules = super().replicate(module, device_ids)
+            else:
+                modules = replicate(module, device_ids,
+                                    copy_parameters=self.copy_parameters, copy_buffers=self.copy_buffers)
+            if self.allow_replication_callback:
+                exec_data_parallel_replication_callback(modules)
+            if self.persistent:
+                self.replicas.extend(modules)
+        else:
+            modules = list(self.replicas)
+        return modules
+
+
+class UserScatteredJacDataParallel(JacDataParallel):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('user_scattered', True)
+        super().__init__(*args, **kwargs)
---
+++ jactorch/parallel/replicate.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# File   : replicate.py
+# Author : Jiayuan Mao
+# Email  : maojiayuan@gmail.com
+# Date   : 19/03/2018
+#
+# This file is part of Jacinle.
+
+import torch.cuda.comm as comm
+
+
+def replicate(network, devices, copy_parameters=False, copy_buffers=False):
+    devices = tuple(devices)
+    num_replicas = len(devices)
+
+    params = list(network.parameters())
+    param_indices = {param: idx for idx, param in enumerate(params)}
+    if not copy_parameters:
+        from torch.nn.parallel._functions import Broadcast
+
+        param_copies = Broadcast(devices)(*params)
+        if len(params) > 0:
+            param_copies = [param_copies[i:i + len(params)]
+                            for i in range(0, len(param_copies), len(params))]
+    else:
+        param_copies = _copy_parameters(params, devices)
+
+    buffers = list(network._all_buffers())
+    buffer_indices = {buf: idx for idx, buf in enumerate(buffers)}
+    if not copy_buffers:
+        buffer_copies = comm.broadcast_coalesced(buffers, devices)
+    else:
+        buffer_copies = _copy_parameters(buffers, devices)
+
+    modules = list(network.modules())
+    module_copies = [[] for device in devices]
+    module_indices = {}
+
+    for i, module in enumerate(modules):
+        module_indices[module] = i
+        for j in range(num_replicas):
+            replica = module.__new__(type(module))
+            replica.__dict__ = module.__dict__.copy()
+            replica._parameters = replica._parameters.copy()
+            replica._buffers = replica._buffers.copy()
+            replica._modules = replica._modules.copy()
+            module_copies[j].append(replica)
+
+    for i, module in enumerate(modules):
+        for key, child in module._modules.items():
+            if child is None:
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._modules[key] = None
+            else:
+                module_idx = module_indices[child]
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._modules[key] = module_copies[j][module_idx]
+        for key, param in module._parameters.items():
+            if param is None:
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._parameters[key] = None
+            else:
+                param_idx = param_indices[param]
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._parameters[key] = param_copies[j][param_idx]
+        for key, buf in module._buffers.items():
+            if buf is None:
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._buffers[key] = None
+            else:
+                buffer_idx = buffer_indices[buf]
+                for j in range(num_replicas):
+                    replica = module_copies[j][i]
+                    replica._buffers[key] = buffer_copies[j][buffer_idx]
+
+    return [module_copies[j][0] for j in range(num_replicas)]
+
+
+def _copy_parameters(params, devices):
+    results = []
+    for i, d in devices:
+        if i == 0:
+            results.append(params.copy())
+        else:
+            results.append([p.cuda(d) for p in params])
+    return results
