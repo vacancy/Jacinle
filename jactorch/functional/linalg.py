4fe95c12d2b96df7ae6a9897ca625e694da91603
Jiayuan Mao <maojiayuan@gmail.com>
2018-05-03 20:08:54 +0800

    [master] add jactorch.logsumexp

--- jactorch/functional/linalg.py
+++ jactorch/functional/linalg.py
@@ -6,8 +6,14 @@
 # 
 # This file is part of Jacinle.
 
-__all__ = ['normalize']
+import torch.nn.functional as F
+
+__all__ = ['normalize', 'logsumexp']
 
 
 def normalize(a, p=2, dim=-1, eps=1e-8):
     return a / a.norm(p, dim=dim, keepdim=True).clamp(min=eps)
+
+
+def logsumexp(inputs, dim=None, keepdim=False):
+    return (inputs - F.log_softmax(inputs)).mean(dim, keepdim=keepdim)
