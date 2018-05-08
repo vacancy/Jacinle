dbc070f115e6ce6c030be80508443d30b81f2695
Jiayuan Mao <maojiayuan@gmail.com>
2018-05-09 01:54:05 +0800

    [master] add vim template

---
+++ .vim-template:.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File   : %FFILE%
+# Author : %USER%
+# Email  : %MAIL%
+# Date   : %MONTH%/%DAY%/%YEAR%
+# 
+# This file is part of Jacinle.
+# Distributed under terms of the %LICENSE% license.
+
+"""
+%HERE%
+"""
