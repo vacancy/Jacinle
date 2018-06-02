1100e9862bed3c59961f4f21f09d7848287e41fd
Jiayuan Mao <maojiayuan@gmail.com>
2018-06-02 21:26:47 +0800

    [rtd] ???

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -88,7 +87,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
