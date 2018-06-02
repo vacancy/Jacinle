dc916b84d567b6f76b84743a5c9293cdc7dc5a10
Jiayuan Mao <maojiayuan@gmail.com>
2018-06-02 20:35:29 +0800

    [master] testing doc conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-
+print(sys.path)
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- General configuration ------------------------------------------------
 
