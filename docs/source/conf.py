268039926f81bf8ef4b76b0846468c585fa59ec0
Jiayuan Mao <maojiayuan@gmail.com>
2018-06-02 21:23:53 +0800

    [rtd] now i hate themes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -87,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pyramid'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
