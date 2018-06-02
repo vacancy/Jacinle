31acdeac3b71697a553842e617eefafbdbdd8d0c
Jiayuan Mao <maojiayuan@gmail.com>
2018-06-02 21:09:57 +0800

    [rtd] seems that i misblamed tensorflow...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,8 +21,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-import sphinx_rtd_theme
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -89,7 +87,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
