aca3a6e6402d6a592cce793e8d68d7bf2ccbdecf
Jiayuan Mao <maojiayuan@gmail.com>
2018-06-15 23:54:57 +0800

    [master] new mask related operations

--- jactorch/nn/mask/functional.py
+++ jactorch/nn/mask/functional.py
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 
 from jactorch.functional.shape import add_dim_as_except
 
-__all__ = ['masked_softmax', 'length_masked_softmax']
+__all__ = ['masked_softmax', 'length_masked_softmax', 'length2mask', 'mask_meshgrid']
 
 
 def masked_softmax(logits, mask, dim=-1, ninf=-1e4):
@@ -28,4 +28,21 @@ def length_masked_softmax(logits, lengths, dim=-1, ninf=-1e4):
     lengths = lengths.unsqueeze(dim)
     mask = rng < lengths
     return masked_softmax(logits, mask, dim=dim, ninf=ninf)
+
+
+def length2mask(lengths, max_length):
+    rng = torch.arange(max_length, dtype=lengths.dtype, device=lengths.device)
+    lengths = lengths.unsqueeze(-1)
+    rng = add_dim_as_except(rng, lengths, -1)
+    mask = rng < lengths
+    return mask.float()
+
+
+def mask_meshgrid(mask, target_dims=2):
+    for i in range(target_dims - 1):
+        f = mask.unsqueeze(-1)
+        g = mask.unsqueeze(-2)
+        mask = f * g
+
+    return mask
 
