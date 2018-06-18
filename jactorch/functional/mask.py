5d2f2bca33d2865456c7e04c88e0190fef23b496
Jiayuan Mao <maojiayuan@gmail.com>
2018-06-18 21:25:36 +0800

    [master] fix add_dim_except

--- jactorch/functional/mask.py
+++ jactorch/functional/mask.py
@@ -8,7 +8,10 @@
 # This file is part of Jacinle.
 # Distributed under terms of the MIT license.
 
-__all__ = ['masked_average']
+__all__ = ['masked_average', 'length_masked_reversed']
+
+import torch
+import numpy as np
 
 
 def masked_average(tensor, mask, eps=1e-8):
@@ -16,3 +19,27 @@ def masked_average(tensor, mask, eps=1e-8):
     mask = mask.float()
     masked = tensor * mask
     return masked.sum() / mask.sum().clamp(min=eps)
+
+
+def length_masked_reversed(tensor, lengths, dim=1):
+    """Reverses sequences according to their lengths.
+    Arguments:
+        tensor (torch.Tensor): padded batch of variable length sequences.
+        lengths (torch.LongTensor): list of sequence lengths
+    Returns:
+        A Variable with the same size as tensor, but with each sequence
+        reversed according to its length.
+    """
+
+    assert dim == 1
+
+    if tensor.size(0) != len(lengths):
+        raise ValueError('tensor incompatible with lengths.')
+    reversed_indices = np.repeat(np.arange(tensor.size(1))[np.newaxis], inputs.size(0), 0)
+    for i, length in enumerate(lengths.cpu().numpy().tolist()):
+        if length > 0:
+            reversed_indices[i, :length] = reversed_indices[i, length-1::-1]
+    reversed_indices = torch.tensor(reversed_indices, dtype=torch.long, device=tensor.device)
+    reversed_inputs = torch.gather(tensor, dim, reversed_indices)
+    return reversed_inputs
+
--- jactorch/functional/shape.py
+++ jactorch/functional/shape.py
@@ -47,8 +47,8 @@ def add_dim(tensor, dim, size):
 def add_dim_as_except(tensor, target, *excepts):
     assert len(excepts) == tensor.dim()
     tensor = tensor.clone()
-    excepts = [e + target.dim() if e < 0 else e for e in range(target.dim())]
-    for i in range(tensor.dim()):
+    excepts = [e + target.dim() if e < 0 else e for e in excepts]
+    for i in range(target.dim()):
         if i not in excepts:
             tensor.unsqueeze_(i)
     return tensor
