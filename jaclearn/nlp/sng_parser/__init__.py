f2410019951dfa444261ca534a2f0dbb4547cc99
Jiayuan Mao <maojiayuan@gmail.com>
2019-05-25 14:21:19 -0400

    [master] import SceneGraphParser

--- jaclearn/nlp/sng_parser/__init__.py
+++ jaclearn/nlp/sng_parser/__init__.py
@@ -3,16 +3,22 @@
 # File   : __init__.py
 # Author : Jiayuan Mao
 # Email  : maojiayuan@gmail.com
-# Date   : 08/21/2018
+# Date   : 07/04/2018
 #
-# This file is part of SceneGraphParser.
+# This file is part of Jacinle.
 # Distributed under terms of the MIT license.
-# https://github.com/vacancy/SceneGraphParser
 
-from .parser import *
-from .utils import *
+from jacinle.utils.vendor import has_vendor, requires_vendors
 
-__version__ = (0, 1, 0)
-__author__ = 'Jiayuan Mao'
-__email__ = 'maojiayuan@gmail.com'
+__all__ = ['tprint', 'Parser', 'get_default_parser', 'parse']
+
+
+if has_vendor('sng_parser'):
+    from sng_parser import tprint, Parser, get_default_parser, parse
+else:
+    from jacinle.utils.meta import make_dummy_func
+    tprint = requires_vendors('sng_parser')(make_dummy_func())
+    Parser = requires_vendors('sng_parser')(make_dummy_func())
+    get_default_parser = requires_vendors('sng_parser')(make_dummy_func())
+    parse = requires_vendors('sng_parser')(make_dummy_func())
 
