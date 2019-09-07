eebce72cf9cab0e7524a1f58805685a74abc9c69
Jiayuan Mao <maojiayuan@gmail.com>
2019-09-07 00:38:46 -0400

    [master] import mldash

---
+++ jaclearn/mldash/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File   : __init__.py
+# Author : Jiayuan Mao
+# Email  : maojiayuan@gmail.com
+# Date   : 09/07/2019
+#
+# This file is part of Jacinle.
+# Distributed under terms of the MIT license.
+
+
+from mldash.client import MLDashClient
+
+__all__ = ['MLDashClient']
