b69f4eb76168449f771ad31dbae6a2b763e56f2b
Jiayuan Mao <maojiayuan@gmail.com>
2020-05-12 17:07:22 -0400

    [master] move inpainting version of gco to graph_cut_inpaint

--- jaclearn/vision/graph_cut_inpaint.py
+++ jaclearn/vision/graph_cut_inpaint.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-# File   : graph_cut.py
+# File   : graph_cut_inpaint.py
 # Author : Jiayuan Mao
 # Email  : maojiayuan@gmail.com
-# Date   : 01/07/2020
+# Date   : 05/12/2020
 #
 # This file is part of Jacinle.
 # Distributed under terms of the MIT license.
 
-from pygco import cut_general_graph, cut_grid_graph, cut_grid_graph_simple
+from pygco_inpaint import cut_simple, cut_simple_vh, cut_from_graph, cut_inpaint
 
