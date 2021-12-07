269eaa98b1d5e9635e327080473bdc0eb8576ec6
Jiayuan Mao <maojiayuan@gmail.com>
2021-12-07 16:15:05 -0500

    update pypatchmatch, sgparser, add KeyboardInterruptContext

--- jactorch/data/dataset.py
+++ jactorch/data/dataset.py
@@ -21,7 +21,7 @@ __all__ = ['IterableDatasetMixin', 'ProxyDataset', 'ListDataset', 'FilterableDat
 class Dataset(object):
     """An abstract class representing a Dataset.
 
-    All other datasets should subclass it. All subclasses should override
+    All other datasets_v1 should subclass it. All subclasses should override
     ``__len__``, that provides the size of the dataset, and ``__getitem__``,
     supporting integer indexing in range from 0 to len(self) exclusive.
     """
